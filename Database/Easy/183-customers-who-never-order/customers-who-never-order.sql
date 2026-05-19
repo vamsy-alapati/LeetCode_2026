95b1c6b18f8ef36787c38401eddbb29f70671ff2
Vamsy Alapati <52825189+vamsy-alapati@users.noreply.github.com>
2026-05-19 10:31:06 -0400

    Time: 737 ms (8.58%) | Memory: 0B (100.00%) - LeetSync

--- Database/Easy/183-customers-who-never-order/customers-who-never-order.sql
+++ Database/Easy/183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT name AS Customers
-  FROM Customers
- WHERE id NOT IN (SELECT DISTINCT customerId FROM Orders);
+SELECT c.name AS Customers
+  FROM Customers c
+LEFT JOIN Orders o on c.id = o.customerId
+ WHERE o.customerId IS NULL;
